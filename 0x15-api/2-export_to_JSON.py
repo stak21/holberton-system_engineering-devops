0535c701f006d3803a1926374fdfe80c80e41d79
stak21 <stakashima21@gmail.com>
2019-05-07 04:37:40 +0000

    feat(0x15): Script: request an employees information and export it in json format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,2 +1,36 @@
 #!/usr/bin/python3
-'''
+"""
+Script: Request from the given api for a given employee ID and export the
+information to json format
+
+"""
+import json
+import requests
+import sys
+
+
+todo_url = "https://jsonplaceholder.typicode.com/todos/"
+users_url = "https://jsonplaceholder.typicode.com/users/"
+user_dict = {}
+if len(sys.argv) >= 2:
+    employee_id = sys.argv[1]
+    try:
+        int(employee_id)
+    except ValueError:
+        print("Argument requires an integer")
+        sys.exit()
+    users_r = requests.get(users_url + employee_id)
+    if users_r.status_code != 200:
+        print("User does not exist")
+        sys.exit()
+    todo_r = requests.get(todo_url)
+    user = json.loads(users_r.text)
+    with open('{}.json'.format(employee_id), mode='w') as employee_file:
+        user_dict["{}".format(employee_id)] = []
+        for task in json.loads(todo_r.text):
+            if task['userId'] == int(employee_id):
+                    user_dict['2'].append(
+                                         dict({"task": task['title'],
+                                              "completed": task['completed'],
+                                               "username": user['username']}))
+    print(user_dict)
