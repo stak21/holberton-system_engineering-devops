57829061208ef3067d7445c24b8a57974696005e
stak21 <stakashima21@gmail.com>
2019-05-10 00:53:17 +0000

    feat(devOps 0x16): Function- Print the top ten hot posts of a subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,2 +1,30 @@
 #!/usr/bin/python3
-'''
+"""
+Function: Queries the Reddit API and print the titles of the top 10 hot posts
+If invalid subreddit, return 0
+"""
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Return the number of subscribers for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}.json".format(subreddit)
+    r = requests.get(url, headers={'User-agent': 'shoji'},
+                     allow_redirects=False)
+    if not r.status_code == 200:
+        print(None)
+        return None
+    data = r.json()
+    try:
+        sub = data.get("data")
+        children = sub.get("children")
+        count = len(children) if len(children) <= 10 else 10
+        for child_number in range(count):
+            subreddit = children[child_number].get("data")
+            print(subreddit.get("title"))
+
+    except Exception as e:
+        print(None)
