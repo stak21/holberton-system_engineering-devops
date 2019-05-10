908d4fea2ae5957404863eb1b7ef95f6a4dfd810
stak21 <stakashima21@gmail.com>
2019-05-10 03:08:35 +0000

    feat(devops 0x16): Function- recurse: returns a list of titles for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,2 +1,37 @@
 #!/usr/bin/python3
-'''
+"""
+Using recursion, navigate through all the listing and return the titles of each
+listing
+if the subreddit is not found return 0
+"""
+import json
+import requests
+
+
+def recurse(subreddit, after={}):
+    """ Returns a list of titles """
+    listings = []
+    url = "https://www.reddit.com/r/{}.json".format(subreddit)
+    headers = {"User-agent": 'Shoji'}
+    params = {"after": after}
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200:
+        return None
+    try:
+        parent = response.json()
+        children = parent['data']['children']
+        for child in children:
+            listings.append(child['data']['title'])
+
+        if after is None:
+            return listings
+        else:
+            after = parent['data']['after']
+
+            ret = recurse(subreddit, after)
+            if listings:
+                ret.extend(listings)
+            return ret
+    except Exception as e:
+        print(e)
+        return listings
