fcc706beeb4a0245783a68bab26352e9f0d488e3
stak21 <stakashima21@gmail.com>
2019-05-13 02:13:05 +0000

    feat(devops 0x16): New script, prints the number of occurrences of a given keyword in a given subreddits hot section

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Using recursion, navigate through all the listing and print the number of
+keyword occurences
+if the subreddit is not found return 0
+"""
+import re
+import requests
+
+
+def count_words(subreddit, word_list, after={}):
+    """ prints the number of occurences of a given keywords """
+    url = "https://www.reddit.com/r/{}.json".format(subreddit)
+    headers = {"User-agent": 'Shoji'}
+    params = {"after": after}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code != 200:
+        print("")
+        return None
+    if after is None:
+        return {} 
+    try:
+        counts = { k: 0 for k in word_list }
+        parent = response.json()
+        children = parent['data']['children']
+        for child in children:
+            for k, v in counts.items():
+                count = [match.start() for match in re.finditer(k,
+                    child['data']['title'])]
+                counts[k] = counts[k] + len(count)
+
+
+        after_copy = parent['data']['after']
+
+        ret_count = count_words(subreddit, word_list, after_copy)
+        if ret_count:
+            for k in counts.keys():
+                counts[k] = counts[k] + ret_count[k]
+            if after == {}:
+                for name in word_list:
+                    print("{}: {}".format(name, counts[name]))
+        return counts
+
+    except Exception as e:
+        print("failed {}".format(e))
+        return None
