cc81393862239f87a98a62c05ed9185674c86221
stak21 <stakashima21@gmail.com>
2019-05-23 16:15:13 +0000

    fix: added a comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
+# comment - serach and replace typo
 exec { 'sed':
-  command => "/bin/sed -i 's/phpp/php/' /var/www/html/wp-settings.php"
-
+  command => "/bin/sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
 }
