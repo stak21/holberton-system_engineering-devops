818e477d670991f96176b79e5c2ed4ee44d34fe8
stak21 <stakashima21@gmail.com>
2019-05-30 19:50:09 +0000

    Copied it wrong from the test container, should be fine now

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
 # comment - serach and replace limit cap
 exec { 'sed':
-  command => "/bin/sed -i '/holberton soft/c\holberton soft nofile 20' /etc/security/limits.conf && /bin/sed -i '/holberton hard/c\holberton hard nofile 20' /etc/security/limits.conf && /usr/sbin/service nginx restart",
+  command => "/bin/sed -i 's/.*holberton soft.*/holberton soft nofile 20/' /etc/security/limits.conf && /bin/sed -i 's/.*holberton hard.*/holberton hard nofile 20/' /etc/security/limits.conf && /usr/sbin/service nginx restart",
 }
