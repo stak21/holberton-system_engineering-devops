83a8304cdcc0e43e3ce614e72a34aa96d51b2384
stak21 <stakashima21@gmail.com>
2019-05-30 23:31:39 +0000

    replace all digits with holberton in the line

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,8 @@
 # comment - serach and replace limit cap
 exec { 'sed':
-  command => "/bin/sed -i 's/.*holberton soft.*/holberton soft nofile 20/' /etc/security/limits.conf && /bin/sed -i 's/.*holberton hard.*/holberton hard nofile 20/' /etc/security/limits.conf && /usr/sbin/service nginx restart",
+  command => "/bin/sed -Ei  sed -Ei '/holberton/s/[0-9]+/20/g' /etc/security/limits.conf",
+}
+
+exec { 'restart':
+  command => '/usr/sbin/service nginx restart',
 }
