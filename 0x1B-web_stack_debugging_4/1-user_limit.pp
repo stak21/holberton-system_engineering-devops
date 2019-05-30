7fb33edd598f842ee7437a6a50550de8bd6b0f3e
stak21 <stakashima21@gmail.com>
2019-05-30 19:42:47 +0000

    Added the sed command and restarted the nginx server

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,5 @@
 
 # comment - serach and replace limit cap
 exec { 'sed':
-  command => -i '/holberton soft/c\holberton soft nofile 20' /etc/security/limits.conf,
-  command => -i '/holberton hard/c\holberton hard nofile 20' /etc/security/limits.conf,
+  command => "/bin/sed -i '/holberton soft/c\holberton soft nofile 20' /etc/security/limits.conf && /bin/sed -i '/holberton hard/c\holberton hard nofile 20' /etc/security/limits.conf",
 }
