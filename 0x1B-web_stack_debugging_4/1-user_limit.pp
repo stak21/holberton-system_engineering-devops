3e5d6e206c65808f0312d16a2d6205db3569cc4c
stak21 <stakashima21@gmail.com>
2019-05-30 19:45:33 +0000

    failed to restart, fixed it

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
 # comment - serach and replace limit cap
 exec { 'sed':
-  command => "/bin/sed -i '/holberton soft/c\holberton soft nofile 20' /etc/security/limits.conf && /bin/sed -i '/holberton hard/c\holberton hard nofile 20' /etc/security/limits.conf",
+  command => "/bin/sed -i '/holberton soft/c\holberton soft nofile 20' /etc/security/limits.conf && /bin/sed -i '/holberton hard/c\holberton hard nofile 20' /etc/security/limits.conf && /usr/sbin/service nginx restart",
 }
