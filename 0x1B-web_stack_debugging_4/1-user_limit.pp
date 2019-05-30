c5c60ce4c74b81f6c165cdb91eb7f1b1f62f13b6
stak21 <stakashima21@gmail.com>
2019-05-30 19:43:44 +0000

    removed extra line in the beginning

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,3 @@
-
 # comment - serach and replace limit cap
 exec { 'sed':
   command => "/bin/sed -i '/holberton soft/c\holberton soft nofile 20' /etc/security/limits.conf && /bin/sed -i '/holberton hard/c\holberton hard nofile 20' /etc/security/limits.conf",
